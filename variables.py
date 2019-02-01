3f5d12890d8a3e8cbcf28c6f9146085d0018c4b8
Mihail Mihaylov <sadas@adsedsaf.bg>
2019-02-01 16:21:03 +0200

    my first variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# my_string_var = "Hello world"
+# my_string_var2 = 'Hello world'
+
+# my_int_var=1
+# my_float_var=3,14
+
+# my_true_var = True
+# my_false_var = False
+
+
+var1 = var2 = 'sifon'
+
+num1=2
+num2=8
+
